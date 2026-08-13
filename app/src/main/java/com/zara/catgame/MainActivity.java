39fce9ea8ba2c163f288c65e1c63bb065e8c0c48
kamlessmaeel <kamlessmaeel@gmail.com>
2026-08-13 22:48:28 +0300

    Add files via upload

---
+++ app/src/main/java/com/zara/catgame/MainActivity.java
@@ -0,0 +1,3 @@
+package com.zara.catgame;
+import android.app.Activity;import android.os.Bundle;import android.view.View;import android.webkit.WebSettings;import android.webkit.WebView;import android.webkit.WebViewClient;
+public class MainActivity extends Activity { @Override public void onCreate(Bundle b){ super.onCreate(b); getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY); WebView w=new WebView(this); setContentView(w); WebSettings s=w.getSettings(); s.setJavaScriptEnabled(true); s.setDomStorageEnabled(true); w.setWebViewClient(new WebViewClient()); w.loadUrl("file:///android_asset/index.html"); } @Override public void onBackPressed(){ finish(); } }
